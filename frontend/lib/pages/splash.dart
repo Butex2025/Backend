64fcd761d8107f8715cf895752dcda1a5b977b9a
wojtekjopkiewicz <wojciech.jopkiewicz@alumnos.upm.es>
2024-10-17 23:14:31 +0200

    splash page

---
+++ frontend/lib/pages/splash.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/sign_in.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (BuildContext context) => SignIn())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'WWJS',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: const Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'BUTEX',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+            // ElevatedButton(
+            //     onPressed: () {
+            //       Navigator.push(context,
+            //           MaterialPageRoute(builder: (context) => SignIn()));
+            //     },
+            //     child: const Text(
+            //       'Next page',
+            //       style: TextStyle(
+            //         color: Colors.blue,
+            //       ),
+            //     )),
+          ],
+        )));
+  }
+}
