89e4d49c2243aa84cc3807ccb58fe276f9482206
szyda <szydasylwia@protonmail.com>
2024-11-14 22:39:32 +0100

    Refactor product details page, add cart screen draft and routings

---
+++ frontend/lib/pages/cart.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/main_load.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // final double screenWidth = MediaQuery.of(context).size.width;
+    // final double paddingHorizontal = screenWidth * 0.04;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () =>Navigator.pop(context),
+        ),
+        title: Text("Cart"),
+        centerTitle: true,
+      )
+    );
+  }
+}
--- frontend/lib/pages/detail_page.dart
+++ frontend/lib/pages/detail_page.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/cubit/shop_cubit.dart';
 import 'package:frontend/data/model/product.dart';
+import 'package:frontend/pages/cart.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   final ProductModel product;
   final List<ProductModel> list;
+
   const DetailPage({
     super.key,
     required this.product,
@@ -15,47 +17,228 @@ class DetailPage extends StatelessWidget {
   });
 
   @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  List<int> availableSizes = [39, 40, 41];
+  List<Color> availableColors = [Colors.white, Colors.black, Colors.blue];
+  int selectedSize = 40;
+  Color selectedColor = Colors.white;
+
+  @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double paddingHorizontal = screenWidth * 0.04;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back_ios),
           onPressed: () {
             moveBackToMainScreen(
               context,
-              list,
+              widget.list,
             );
           },
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.shopping_cart_outlined),
+            icon: const Icon(Icons.shopping_bag_outlined),
             onPressed: () {
-              // cart
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const Cart()),);
             },
           ),
         ],
+        centerTitle: true,
+        title: const Text("Product Details"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.memory(base64Decode(product.image)),
-          Text(product.name),
-          Text('${product.price} \$'),
-          const Text(
-            'Size',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: paddingHorizontal),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Center(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 20,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                  child: Image.memory(
+                    base64Decode(widget.product.image),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.45, 
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            // Product Details
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.product.name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    '${widget.product.price} \$',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  const SizedBox(height: 8), 
+                  const Text(
+                    'Size',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: availableSizes.map((size) {
+                      bool isSelected = size == selectedSize;
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedSize = size;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: isSelected ? Colors.blue : Colors.grey[300],
+                              shape: BoxShape.circle,
+                              boxShadow: isSelected
+                                  ? [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 10,
+                                        spreadRadius: 1,
+                                        offset: const Offset(0, 4),
+                                      ),
+                                    ]
+                                  : [],
+                            ),
+                            child: CircleAvatar(
+                              radius: 24,
+                              backgroundColor: Colors.transparent,
+                              child: Text(
+                                size.toString(),
+                                style: TextStyle(
+                                  color: isSelected ? Colors.white : Colors.black,
+                                  fontWeight:
+                                      isSelected ? FontWeight.bold : FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'Color',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: availableColors.map((color) {
+                      bool isSelected = color == selectedColor;
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedColor = color;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: color,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: isSelected ? Colors.blue : Colors.transparent,
+                                width: 2,
+                              ),
+                              boxShadow: isSelected
+                                  ? [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 10,
+                                        spreadRadius: 1,
+                                        offset: const Offset(0, 4),
+                                      ),
+                                    ]
+                                  : [],
+                            ),
+                            width: 36,
+                            height: 36,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  const Spacer(),
+                  // Add to Cart Button
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // add to cart logika
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: Colors.blue, 
+                        ),
+                        child: const Text(
+                          'Add To Cart',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
 
-moveBackToMainScreen(BuildContext context, List<ProductModel> list) {
-  final pokeCubit = BlocProvider.of<ShopCubit>(context);
-  pokeCubit.moveBackToMainScreen(list);
+  moveBackToMainScreen(BuildContext context, List<ProductModel> list) {
+    final pokeCubit = BlocProvider.of<ShopCubit>(context);
+    pokeCubit.moveBackToMainScreen(list);
+  }
 }
